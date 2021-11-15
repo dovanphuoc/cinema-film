53f9a2684798312a2910c738539b474feebe2ded
dovanphuoc <dovanphuocht@gmail.com>
2021-11-15 10:04:11 +0700

    change text when dark and light mode

--- src/features/Movies/MovieTitle/MovieTitle.js
+++ src/features/Movies/MovieTitle/MovieTitle.js
@@ -2,29 +2,34 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import ROUTES from '../../../routers'
 import { css } from '@emotion/css'
+import './MovieTitle.css'
+import { useSelector } from 'react-redux'
 
 const nameTitle = css`
   line-height: 1.6;
 `
 
-const title = css`
-  color: #000000de;
-`
+const title = css``
 
 const desc = css`
-  color: #000000de;
   margin-bottom: 5px;
   font-size: 1.6rem;
 `
 const MovieTitle = ({ movies }) => {
+  const theme = useSelector((state) => state.theme)
   return (
     <>
-      <h2 className={nameTitle}>
-        <Link to={ROUTES[10].path} className={title}>
+      <h2 className={`${nameTitle} ${theme === 'light' ? 'dark' : 'light'}`}>
+        <Link
+          to={ROUTES[10].path}
+          className={`${title} ${theme === 'light' ? 'dark' : 'light'}`}
+        >
           {movies.name}
         </Link>
       </h2>
-      <p className={desc}>{movies.description}</p>
+      <p className={`${desc} ${theme === 'light' ? 'dark' : 'light'}`}>
+        {movies.description}
+      </p>
     </>
   )
 }
